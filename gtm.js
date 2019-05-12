3b69e870578e40df789b2145e0c07badc3b4b06a
大橋史貴 <fumitaka0816bridge@yahoo.co.jp>
2019-05-12 10:51:16 +0900

    アクセス解析のため、GoogleAnalytics設置

---
+++ gtm.js
@@ -0,0 +1,6 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140015661-1"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-140015661-1');
